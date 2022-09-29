6f090a0e16861d9405871f87d6078716d927af83
derek089 <nguyen.hai.dang@icloud.com>
2022-09-29 20:02:22 +0700

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/todoapp'  // This is whatever your path from the root is
+        ? '/todoapp/'  // This is whatever your path from the root is
         : '/'
 }
